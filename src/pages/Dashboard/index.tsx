fcd123f6025b2477b612201c2e88e25dda807cd6
Paulo Vitor Souza <paulo225vitor@gmail.com>
2021-04-06 19:18:37 -0300

    API pt1

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
 import githubExplorer from '../../assets/githubExplorer.svg';
 
 import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, serRepositories] = useState([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    // Add de um novo repo
+    // consumir api do github
+    event.preventDefault();
+    const response = await api.get(`repos/${newRepo}`);
+  }
+
   return (
     <>
       <img src={githubExplorer} alt="Github Explorer" />
       <Title>Explore seus repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit"> Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/40959653?v=4"
-            alt="Paulo Vitor"
-          />
-          <div>
-            <strong>Rocketseat/Unform</strong>
-            <p>Easy etc etc etc</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
         <a href="teste">
           <img
             src="https://avatars.githubusercontent.com/u/40959653?v=4"
